42075b0a2938deb73ed5b0405f4a30685bc155ee
Seth King <sethkingwriter@gmail.com>
2022-05-25 19:12:30 -0500

    delete, update routes updated

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const Handlebars = require('handlebars')
-
 const path = require('path')
 const app = express()
 
